04022d0a64ccb56f8dc6651d98f69891388adda8
rafibuldan <blazblue19@gmail.com>
2019-12-15 13:45:33 +0700

    awal

---
+++ home.php
@@ -0,0 +1,11 @@
+<div class="page-header">
+    <h1>Sistem Informasi Geografis Sistem Informasi Geografis</h1>
+</div>
+<p>Takengon sebuah ibukota dari kabupaten Aceh tengah yang merupakan wilayah Gayo ini merupakan daerah kopi, hampir semua masyarakatnya adalah petani kopi, dari yang mulai PNS semacam guru sampai dengan aparat seperti tentara dan polisi juga banyak yang memiliki kebun kopi.</p>
+<p>Jika kamu datang ke-Takengon maka akan sangat mudah kamu menjumpai kedai kopi dari yang kelas emperan sampai dengan kelas cafe semuanya marak di kota kopi yang udaranya dingin dan berbukit ini, tapi ada satu hal yang bisa kamu nikmati selain kopi di kota ini.</p>
+<p>Gayo Highland atau orang biasa menyebutnya dengan Bur Gayo, adalah sebuah tempat yang berada di bukit di pinggiran kota takengon, dari sana kamu bisa menikmati Indahnya danau Lut Tawar yang nampak jelas, serta jika kamu ingin melihat bagaimana kota takengon dari ketinggian maka datanglah ke Gayo Highland.</p>
+<p>Gayo Highland adalah sebuah tulisan yang apabila kamu memasuki kota takengon maka akan nampak dengan jelas tulisan tersebut, untuk rute tak jauh dari kota takengon cukup kamu menuju pasar ikan saja setelah itu tanya kepada orang di sekitar. Rute yang mudah karna jalurnya hanya satu saja *setau saya.</p>
+<p>Walaupun dalam perjalanan kamu akan merasakan betapa tempat seindah ini kok bisa jalannya tak di bangun dengan sempurna, walaupun sekarang juga dalam tahap pengelolaan, semoga saja Gayo Highland bisa memberikan kenyamanan di kemudian hari</p>
+<p>Satu hal yang saya sendiri sempat kecewa, banyak coretan-coretan menggunakan pilox dan spidol di sepanjang tulisan Gayo Highland. Kurangnya pengawasan dan kesadaran dari Individu yang tak bertanggung jawab membuat Gayo Highland berkurang keindahannya.</p>
+<p>Tapi walau seperti itu, kamu tetap saja akan menemukan keindahan yang memanjakan mata, danau lut tawar yang indah asri dan sejuknya pepohonan pinus di sekitar Gayo Highland.</p>
+<p>Tapi walau seperti itu, kamu tetap saja akan menemukan keindahan yang memanjakan mata, danau lut tawar yang indah asri dan sejuknya pepohonan pinus di sekitar Gayo Highland.</p>
